239de15e17e16a6a737b7817a18f3d2a34e0123f
Evans Andawa <evansandawa2@gmail.com>
2025-09-19 22:45:18 +0300

    First Commit

---
+++ admin/src/context/AppContext.jsx
@@ -0,0 +1,54 @@
+import { createContext } from "react";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AppContext = createContext()
+
+const AppContextProvider = (props) => {
+
+    const currency = '$';
+
+    const calculateAge = (dob) => {
+        const today = new Date()
+        const birthDate = new Date(dob)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+        return age
+    }
+
+    const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+
+    const slotDateFormat = (slotDate) => {
+        const dateArray = slotDate.split("_");
+        const day = dateArray[0];
+        const month = months[Number(dateArray[1]) - 1]; // -1 because months array is 0-indexed
+        const year = dateArray[2];
+        return `${day} ${month} ${year}`;
+    };
+
+    const value = {
+        calculateAge,
+        slotDateFormat,
+        currency
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider
